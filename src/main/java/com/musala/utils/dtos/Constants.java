2002c22d517a779d66dc5996ec8be4cf28b15646
Marvelous Mashamba <marvellousmashamba.mm@gmail.com>
2023-04-18 09:57:11 +0200

    Configure constants

---
+++ src/main/java/com/musala/utils/dtos/Constants.java
@@ -0,0 +1,20 @@
+package com.musala.utils.dtos;
+
+public class Constants {
+
+    public static final int SUCCESS_INT_VALUE = 200;
+    public static final int FAILURE_INT_VALUE = 400;
+
+    public static final int NOT_FOUND_INT_VALUE = 404;
+    public static final String DEFAULT_LOCALE_LANGUAGE = "en";
+    public static final String INVALID_PARAM = "INVALID_PARAM";
+    public static final String LOCALE_LANGUAGE = "Language";
+
+    public static final String DUPLICATE_ENTRY = "DUPLICATE_ENTRY";
+    public static final String DRONE_FLEET_SIZE_VIOLATION = "DRONE_FLEET_SIZE_VIOLATION";
+    public static final int CREATE_SUCCESS_INT_VALUE = 201;
+
+    private Constants() {
+    }
+
+}
